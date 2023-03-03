dd9d4dd31ba31e49bb7a6d56367a1029208697d5
loinsir <a9327370@gmail.com>
2023-03-03 18:38:50 +0900

    build: testflight 배포용

--- Runway-iOS/Sources/Scenes/Main/ViewModels/ShowRoomDetailReactor.swift
+++ Runway-iOS/Sources/Scenes/Main/ViewModels/ShowRoomDetailReactor.swift
@@ -1,54 +1,64 @@
+////
+////  ShowRoomDetailReactor.swift
+////  Runway-iOS
+////
+////  Created by 김인환 on 2023/02/27.
+////
 //
-//  ShowRoomDetailReactor.swift
-//  Runway-iOS
+//import Foundation
 //
-//  Created by 김인환 on 2023/02/27.
+//import ReactorKit
+//import RxFlow
+//import RxSwift
+//import RxCocoa
 //
-
-import Foundation
-
-import ReactorKit
-import RxFlow
-import RxSwift
-import RxCocoa
-
-import Alamofire
-
-
-final class ShowRoomDetailReactor: Reactor, Stepper {
-    // MARK: - Events
-    
-    struct State {
-    }
-    
-    enum Action {
-
-    }
-    
-    enum Mutation {
-
-    }
-    
-    // MARK: - Properties
-    
-    var steps = PublishRelay<Step>()
-    
-    let initialState: State
-    let provider: ServiceProviderType
-    
-    private let disposeBag = DisposeBag()
-    
-    // MARK: - initializer
-    init(provider: ServiceProviderType) {
-        self.provider = provider
-        self.initialState = State()
-    }
-    
-    func mutate(action: Action) -> Observable<Mutation> {
-        
-    }
-    
-    func reduce(state: State, mutation: Mutation) -> State {
-
-    }
-}
+//import Alamofire
+//
+//
+//final class ShowRoomDetailReactor: Reactor, Stepper {
+//    // MARK: - Events
+//    
+//    enum Action {
+//        case viewWillAppear
+//    }
+//    
+//    enum Mutation {
+//        case setShowRoomImageUrl(String)
+//    }
+//    
+//    struct State {
+//        var showRoomImageURL: String? = nil
+//    }
+//    
+//    // MARK: - Properties
+//    
+//    var steps = PublishRelay<Step>()
+//    
+//    let initialState: State
+//    let provider: ServiceProviderType
+//    
+//    private let disposeBag = DisposeBag()
+//    
+//    let storeId: Int
+//    
+//    // MARK: - initializer
+//    init(provider: ServiceProviderType, storeId: Int) {
+//        self.provider = provider
+//        self.initialState = State()
+//        self.storeId = storeId
+//    }
+//    
+//    func mutate(action: Action) -> Observable<Mutation> {
+//        switch action {
+//        case .viewWillAppear:
+//            return Observable.concat([
+//                provider.showRoomService.storeDetail(storeId: storeId)
+//                    .data().decode(type: <#T##Decodable.Protocol#>, decoder: JSONDecoder())
+//            ])
+//        }
+//    }
+//    
+//    func reduce(state: State, mutation: Mutation) -> State {
+//
+//    }
+//}
