b90e22d962d1dd9aef862ddbb952581a6cd286dc
loinsir <a9327370@gmail.com>
2023-07-07 02:54:02 +0900

    chore: ci_script 재수정

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -4,29 +4,10 @@
 #  Runway-iOS
 #
 #  Created by 김인환 on 2023/02/15.
-#  
+#
+brew install cocoapods
 
-brew update
-brew install rbenv ruby-build
-rbenv install -l
-rbenv install 3.1.3
-rbenv global 3.1.3
+pod cache clean --all
 
-echo ">>> SETUP LOCAL GEM PATH"
-echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
-echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
-export GEM_HOME=$HOME/gems
-export PATH="$GEM_HOME/bin:$PATH"
+pod install
 
-gem install bundler
-bundle install
-brew install git-lfs
-
-git lfs install --skip-smudge
-git lfs pull
-git lfs install --force
-
-bundle exec pod cache clean NMapsMap
-
-# Install dependencies you manage with CocoaPods.
-bundle exec pod install --repo-update
